cf05a1b20cbfeaa0cab920c112b6faa2437f764e
Alberte Mozo <albertegz@gmail.com>
2023-07-05 23:01:49 +0200

    Remove redundant method

--- tests/Application/AddShould.php
+++ tests/Application/AddShould.php
@@ -14,23 +14,15 @@ class AddShould extends TestCase
     /**
      * @test
      */
-    public function beCallable(): void
-    {
-        $this->expectNotToPerformAssertions();
-        $repository = new InMemoryTodoRepository();
-        $add = new Add($repository);
-        $add('Laundry');
-    }
-
-    /**
-     * @test
-     */
     public function addAnItem(): void
     {
         $repository = new InMemoryTodoRepository();
         $add = new Add($repository);
         $add('Laundry');
 
-        $this->assertNotEmpty(array_filter($repository->all(), static fn(Todo $todo) => $todo->description() === 'Laundry'));
+        $this->assertNotEmpty(array_filter(
+            $repository->all(),
+            static fn(Todo $todo) => $todo->description() === 'Laundry'
+        ));
     }
 }
